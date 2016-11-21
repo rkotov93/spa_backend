f9240f72e531f71005d51ae78b2cb3b7573ad1df
Ruslan Kotov <rkotov93@gmail.com>
2016-11-21 02:29:38 +0200

    [Posts] Add controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  protected
+
+  def current_user
+    User.first
+  end
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,12 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :body, :author
+  attributes :id, :title, :body, :author
+  attribute :errors, if: :errors_presented?
 
   def author
-    user.name
+    object.user.name
+  end
+
+  def errors_presented?
+    object.errors.present?
   end
 end
