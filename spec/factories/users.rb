92cacdc0ae02a5cc510282045d93320b2090d1af
Ruslan Kotov <rkotov93@gmail.com>
2016-11-23 12:23:03 +0200

    Fix rubocop offences

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
 FactoryGirl.define do
   factory :user do
-    name { Faker::Name.name   }
+    name { Faker::Name.name }
     email { Faker::Internet.email }
   end
 end
