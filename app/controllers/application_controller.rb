c1306b62d71fd144f736f299797f9b58ad9a0fb4
Ruslan Kotov <rkotov93@gmail.com>
2017-01-13 15:17:12 +0200

    Add tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
   include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    render status: :unauthorized
+  end
 end
--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -2,8 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let(:author) { create :user }
   let(:user) { create :user }
-  let!(:first_post) { create :post, user: user }
+  let!(:first_post) { create :post, user: author }
 
   describe 'GET /posts' do
     before { get api_v1_posts_path }
@@ -30,7 +31,7 @@ RSpec.describe 'Posts', type: :request do
     context 'with valid params' do
       before do
         post_params = { title: new_post.title, body: new_post.body }
-        post api_v1_posts_path, headers: { authorization: jwt_for(user) }, params: { post: post_params }
+        post api_v1_posts_path, headers: { authorization: jwt_for(author) }, params: { post: post_params }
       end
 
       it 'creates and returns new post' do
@@ -43,12 +44,12 @@ RSpec.describe 'Posts', type: :request do
     context 'with invalid params' do
       let(:post_params) { { title: 'a' * 121, body: new_post.body } }
 
-      before { post api_v1_posts_path, headers: { authorization: jwt_for(user) }, params: { post: post_params } }
+      before { post api_v1_posts_path, headers: { authorization: jwt_for(author) }, params: { post: post_params } }
 
       it 'returns post with validation errors' do
         expect(Post.count).to eq posts_count
         expect(response).to have_http_status(406)
-        invalid_post = Post.new(post_params.merge(user: user))
+        invalid_post = Post.new(post_params.merge(user: author))
         invalid_post.valid?
         expect(response.body).to eq PostSerializer.new(invalid_post).to_json
       end
@@ -56,26 +57,40 @@ RSpec.describe 'Posts', type: :request do
   end
 
   describe 'PATCH /posts/:id' do
-    context 'with valid params' do
+    context 'by authorized user' do
+      context 'with valid params' do
+        before do
+          patch api_v1_post_path(first_post), headers: { authorization: jwt_for(author) },
+                                              params: { post: { title: 'New title' } }
+        end
+
+        it 'updates post title' do
+          expect(response).to have_http_status(200)
+          expect(first_post.reload.title).to eq 'New title'
+        end
+      end
+
+      context 'with invalid params' do
+        before do
+          patch api_v1_post_path(first_post), headers: { authorization: jwt_for(author) },
+                                              params: { post: { title: 'a' * 121 } }
+        end
+
+        it 'does not updates post title' do
+          expect(response).to have_http_status(406)
+          expect(first_post.reload.title).to eq first_post.title
+        end
+      end
+    end
+
+    context 'by non-authorized user' do
       before do
         patch api_v1_post_path(first_post), headers: { authorization: jwt_for(user) },
                                             params: { post: { title: 'New title' } }
       end
 
-      it 'updates post title' do
-        expect(response).to have_http_status(200)
-        expect(first_post.reload.title).to eq 'New title'
-      end
-    end
-
-    context 'with invalid params' do
-      before do
-        patch api_v1_post_path(first_post), headers: { authorization: jwt_for(user) },
-                                            params: { post: { title: 'a' * 121 } }
-      end
-
-      it 'does not updates post title' do
-        expect(response).to have_http_status(406)
+      it 'returns unauthorized status' do
+        expect(response).to have_http_status(401)
         expect(first_post.reload.title).to eq first_post.title
       end
     end
@@ -84,11 +99,22 @@ RSpec.describe 'Posts', type: :request do
   describe 'DELETE /posts/:id' do
     let!(:posts_count) { Post.count }
 
-    before { delete api_v1_post_path(first_post), headers: { authorization: jwt_for(user) } }
+    context 'by authorized user' do
+      before { delete api_v1_post_path(first_post), headers: { authorization: jwt_for(author) } }
 
-    it 'destroys post' do
-      expect(response).to have_http_status(200)
-      expect(Post.count).to eq posts_count - 1
+      it 'destroys post' do
+        expect(response).to have_http_status(200)
+        expect(Post.count).to eq posts_count - 1
+      end
+    end
+
+    context 'by non-authorized user' do
+      before { delete api_v1_post_path(first_post), headers: { authorization: jwt_for(user) } }
+
+      it 'returns unauthorized status' do
+        expect(response).to have_http_status(401)
+        expect(Post.count).to eq posts_count
+      end
     end
   end
 end
