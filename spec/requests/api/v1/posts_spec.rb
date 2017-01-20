30281a0debda314519d7edab0105e64606f56dea
Ruslan Kotov <rkotov93@gmail.com>
2017-01-20 12:19:47 +0200

    Fix tests

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -12,7 +12,10 @@ RSpec.describe 'Posts', type: :request do
 
       it 'returns list of posts' do
         expect(response).to have_http_status(200)
-        expect(response.body).to eq [PostSerializer.new(first_post)].to_json
+        expect(response.body).to eq({
+          posts: [PostSerializer.new(first_post)],
+          meta: { current_page: 1, total_pages: 1, total_count: 1 }
+        }.to_json)
       end
     end
 
@@ -26,7 +29,10 @@ RSpec.describe 'Posts', type: :request do
 
       it 'returns list of posts on second page' do
         expect(response).to have_http_status(200)
-        expect(response.body).to eq [PostSerializer.new(another_post)].to_json
+        expect(response.body).to eq({
+          posts: [PostSerializer.new(another_post)],
+          meta: { current_page: 2, total_pages: 2, total_count: 2 }
+        }.to_json)
       end
     end
   end
@@ -36,7 +42,7 @@ RSpec.describe 'Posts', type: :request do
 
     it 'returns requested post' do
       expect(response).to have_http_status(200)
-      expect(response.body).to eq PostSerializer.new(first_post).to_json
+      expect(response.body).to eq({ post: PostSerializer.new(first_post) }.to_json)
     end
   end
 
@@ -53,7 +59,7 @@ RSpec.describe 'Posts', type: :request do
       it 'creates and returns new post' do
         expect(Post.count).to eq posts_count + 1
         expect(response).to have_http_status(200)
-        expect(response.body).to eq PostSerializer.new(Post.last).to_json
+        expect(response.body).to eq({ post: PostSerializer.new(Post.last) }.to_json)
       end
     end
 
@@ -67,7 +73,7 @@ RSpec.describe 'Posts', type: :request do
         expect(response).to have_http_status(406)
         invalid_post = Post.new(post_params.merge(user: author))
         invalid_post.valid?
-        expect(response.body).to eq PostSerializer.new(invalid_post).to_json
+        expect(response.body).to eq({ post: PostSerializer.new(invalid_post) }.to_json)
       end
     end
   end
