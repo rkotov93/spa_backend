edd8522def961d30bd17a32f6cec54338fa9f25b
Ruslan Kotov <rkotov93@gmail.com>
2017-01-20 12:02:41 +0200

    Fix rubocop issues

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,5 @@
+# frozen_string_literal: true
 class AvatarUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -44,5 +44,4 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
@@ -6,6 +7,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Figaro.env.aws_secret_access_key,
     region:                'eu-central-1'
   }
-  config.fog_directory  = 'rkotov93-spa-bucket'                          # required
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  config.fog_directory  = 'rkotov93-spa-bucket' # required
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
